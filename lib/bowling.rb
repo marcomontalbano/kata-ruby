eb1eb03c8fdf8514542d238dc1026849c93ddcc9
Marco Montalbano <mmontalbano@sopragroup.com>
2013-10-08 11:11:59 +0200

    Added tests and refactored code.

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,12 +1,16 @@
 module Bowling
     class Player
 
+        FRAMES = 10
+        
         def initialize
-            @rolls = []
+            @rolls       = Array.new(21) { 0 }
+            @currentRoll = 0
         end
 
         def rolls pins
-            @rolls.push pins
+            @rolls[ @currentRoll ] = pins
+            @currentRoll += 1
             self
         end
 
@@ -14,9 +18,17 @@ module Bowling
             score = 0
 
             i = 0
-            while i < @rolls.length do
-                score += @rolls[i]
-                i += 1
+            (1..FRAMES).each do
+                if (@rolls[i] == 10)
+                    score += 10 + @rolls[i+1] + @rolls[i+2]
+                    i += 1
+                elsif (@rolls[i] + @rolls[i+1] == 10)
+                    score += 10 + @rolls[i+2]
+                    i += 2
+                else
+                    score += @rolls[i] + @rolls[i+1]
+                    i += 2
+                end
             end
 
             score
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -27,10 +27,43 @@ module Bowling
         end
 
         it "can spares" do
-            #pending
             player.rolls(5).rolls(5).rolls(2)
             knockOverPins(0, 17)
             player.score.should == 14
+        end
+
+        it "can strike" do
+            player.rolls(10).rolls(2).rolls(5)
+            knockOverPins(0, 16)
+            player.score.should == 24
+        end
+
+        it "can do a perfect game" do
+            knockOverPins(10, 12)
+            player.score.should == 300
+        end
+
+        it "can play bowling" do
+            player
+                .rolls(5)
+                .rolls(3)
+                .rolls(5)
+                .rolls(1)
+                .rolls(6)
+                .rolls(4)
+                .rolls(8)
+                .rolls(1)
+                .rolls(10)
+                .rolls(5)
+                .rolls(4)
+                .rolls(6)
+                .rolls(4)
+                .rolls(10)
+                .rolls(10)
+                .rolls(6)
+                .rolls(4)
+                .rolls(4)
+            player.score.should == 149
         end
     end
 end
