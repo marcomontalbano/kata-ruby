905fa698e793a355e17a2c0fc87d32e5f72a0ea4
Marco Montalbano <marco.montalbano@sopragroup.com>
2013-11-14 10:27:08 +0100

    2013.11.11 - Added tests.

--- lib/ruby_tt.rb
+++ lib/ruby_tt.rb
@@ -1,3 +1,47 @@
 module RubyTT
-  
-end
+
+  module SameClassDeclaration
+
+    module Utils
+      def hi
+        "Hello World!"
+      end
+    end
+
+    ##
+    ## Extending (extend) the class means add functionalities to its eigenclass.
+    ##
+
+    # method 1
+    class MyClass1
+      class << self # access to its eigenclass.
+        include Utils
+      end
+    end
+
+    # method 2
+    class MyClass2
+      extend Utils
+    end
+
+    # method 3
+    class MyClass3
+    end
+    MyClass3.extend Utils
+
+    
+    # Method A
+    class MyClassA
+      include Utils
+    end
+
+    # method B
+    class MyClassB
+      def hi
+        "Hello World!"
+      end
+    end
+
+  end #</SameClassDeclaration>
+
+end #</RubyTT>
--- spec/ruby_tt_spec.rb
+++ spec/ruby_tt_spec.rb
@@ -2,5 +2,22 @@ require "rspec"
 require "./lib/ruby_tt"
 
 module RubyTT
-    
+  
+  describe "Class" do
+
+    context "regarding the declaration" do
+      it "could be declared in different modes" do
+        SameClassDeclaration::MyClass1.hi.should == "Hello World!"
+        SameClassDeclaration::MyClass2.hi.should == "Hello World!"
+        SameClassDeclaration::MyClass3.hi.should == "Hello World!"
+        
+        expect{SameClassDeclaration::MyClassA.hi}.to raise_error ::NoMethodError
+        SameClassDeclaration::MyClassA.new.hi.should == "Hello World!"
+        expect{SameClassDeclaration::MyClassB.hi}.to raise_error ::NoMethodError
+        SameClassDeclaration::MyClassB.new.hi.should == "Hello World!"
+      end
+    end
+
+  end
+  
 end
